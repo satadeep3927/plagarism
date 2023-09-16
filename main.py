efeccc511273754b9e90d38bc22067a96c062a11
Satdeep Dasgupta <satadeep3927@gmail.com>
2023-09-16 10:54:45 +0530

    REPOSITORY INTIATED

---
+++ main.py
@@ -0,0 +1,5 @@
+from plagarism import main_function
+
+text: str = """Commits serve as the tangible building blocks of a programmer’s craft. They act as the icing on the cake of code, and when written correctly, they bring substantial value. A well-written commit message becomes indispensable because they provide context — otherwise a commit message wouldn’t be needed at the first place."""
+
+print(main_function(text))
